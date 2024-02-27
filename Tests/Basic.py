5c7d8b7589f115133240833a56766ca9e4d5b644
mareknovakz <mareknovak.z@gmail.com>
2024-02-27 13:11:28 +0100

    13:11 - 27.02.2024

--- Tests/Basic.py
+++ Tests/Basic.py
@@ -4,12 +4,12 @@ import os
 
 class BasicTools:
     def __init__(self):
-        self.driver = webdriver.Firefox() #Definuje
+        self.driver = webdriver.Firefox() #Definuje prohlizec Firefox
 
     def open_html_file(self, file_name):#Na vstupu se definuje testovany html soubor
-        current_directory = os.path.dirname(__file__) #Definuje cestu
-        file_path = os.path.join(current_directory, '..', file_name) #Nalezne absolutni cestu k souboru na disku
-        self.driver.get('file://' + file_path)# "secte" absolutni cestu ke slozce s cestou relativni,
+        current_directory = os.path.dirname(__file__)
+        file_path = os.path.join(current_directory, '..', file_name) #Moznost upravit pri zmene lokality html souboru
+        self.driver.get('file://' + file_path)
 
     def run_test(self,file_name):
         self.open_html_file(file_name) #Zajisti trvale otevreni prohlizece Firefox
