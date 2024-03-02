dcfa1635943c98112241ebef3baa02e1abffb600
mareknovakz <138372976+mareknovakz@users.noreply.github.com>
2024-03-02 17:51:47 +0100

    Add files via upload

--- Tests/Basic.py
+++ Tests/Basic.py
@@ -1,17 +1,30 @@
 #Class definuje zakladni metody pro praci s automatizovanymi nastroji
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 import os
+import logging
 
 class BasicTools:
     def __init__(self):
-        self.driver = webdriver.Firefox() #Definuje prohlizec Firefox
+        try:
+            self.driver = webdriver.Firefox() #Deklarace Firefoxu jako vychoziho prohlizece
+            #self.driver = webdriver.Chrome()
+            logging.basicConfig(level=logging.INFO)
+            self.logger = logging.getLogger(__name__)
+        except WebDriverException as e:
+          self.logger.error("Nastala chyba pri inicializaci prohlizece {e}")
 
+    #Otevreni html souboru
     def open_html_file(self, file_name):#Na vstupu se definuje testovany html soubor
-        current_directory = os.path.dirname(__file__)
-        file_path = os.path.join(current_directory, '..', file_name) #Moznost upravit pri zmene lokality html souboru
-        self.driver.get('file://' + file_path)
+        try:
+            #Pokud se soubor nenachazi v root slozce je nutne misto nazvu zadat jeho relativni cestu
+            cwd = os.getcwd()  # aktualni adresar
+            path = os.path.join(cwd, file_name)  # Spojuje relativni cestu a absolutni cestu k adresari
+            self.driver.get("file:///" + path)  # Otevreni souboru
+        except WebDriverException as e:
+            self.logger.error("Nastala chyba při otevírání souboru: {e}")
 
-    def run_test(self,file_name):
-        self.open_html_file(file_name) #Zajisti trvale otevreni prohlizece Firefox
+        return self.driver
+
 
 
--- Tests/LoginFill.py
+++ Tests/LoginFill.py
@@ -1,13 +1,37 @@
 #Trida definuje metody pro vyplneni formulare
-from Tests.Basic import BasicTools as open
+import logging
+from Tests.Basic import BasicTools
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 class Fill:
-    def __init__(self,file_name):
-        open.run_test(self,file_name) #Zajisti travle otevreni okna
-        #open.open_html_file(self) #Okno jen "problikne"
+    def __init__(self, html_file):
+        self.basic_tools = BasicTools()  # Vytvoření instance třídy BasicTools
+        self.driver = self.basic_tools.driver  # Získání ovladače
+        self.basic_tools.open_html_file(html_file)  # Otevření HTML souboru
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
 
-    def fill_form(self, username,email, password, password_again): #Na vstupu jsou pozadovana data k vyplneni
-        self.driver.find_element_by_id('username').send_keys(username)
-        self.driver.find_element_by_id('email').send_keys(email)
-        self.driver.find_element_by_id('password').send_keys(password)
-        self.driver.find_element_by_id('password_again').send_keys(password_again)
+#Metoda nacte matici dat
+#1. sloupec obsahuje jmena prvku
+#2. sloupec obsahuje data k vyplneni
+    def Fill_strings(self, data):
+        for row in data: #Cyklus postupne dohleda a doplni jeden element
+            if len(row) != 2:
+                self.logger.error("Chyba: Matice musi obsahovat 2 sloupce se jmenem prvku a s daty.")
+                return
+
+            name, value = row
+            try:
+                element = self.driver.find_element(By.NAME, name)
+                element.send_keys(value)
+            except NoSuchElementException as e:
+                self.logger.error(f"Prvek s jménem '{name}' nebyl nalezen:{e}")
+
+    def Button_click(self, name_button):
+        try:
+            self.driver.find_element(By.NAME, name_button).click()
+        except NoSuchElementException as e:
+            self.logger.error(f"Prvek s jménem '{name_button}' nebyl nalezen: {e}")
+
+
